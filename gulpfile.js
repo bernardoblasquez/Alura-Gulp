4759d5a33ec7baaa90f46572b340ec9365970ee6
Bernardo Blasquez Taucei <Bernardo Blasquez Taucei>
2017-05-19 16:47:36 -0300

    configuração da task clean, criação de dependencias e ordenação de chamandas das streams com o return

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,34 @@
-// Importação de bibliotecas
+// Importação de modulos
 // procura na pasta do node models a pasta gulp
 var gulp = require('gulp'),
-    imagemin = require('gulp-imagemin');
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean');
 
-gulp.task('build-img', function(){
+
+// BUILD: minifica arquivos de imagem
+gulp.task('build-img', ['copy'], function(){
 
     gulp.src('projeto/src/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('projeto/src/img02'));
+});
 
+// CLEAN: apaga pasta pré-dfinida
+gulp.task('clean', function(){
+
+    return gulp.src('distribuicao-projeto')
+            .pipe(clean('distribuicao-projeto'))
+});
+
+// COPY: copia arquivo ou pasta para um destino definido
+gulp.task('copy', ['clean'], function() {
+   return gulp.src('projeto/**/*')
+        .pipe(gulp.dest('distribuicao-projeto'))
+});
+
+
+// Formato da configuração de uma task
+gulp.task('nomeDaTarefa', function() {
+    // código da sua tarefa aqui
+    // -- pipe() - liga fluxos de dados
 });
