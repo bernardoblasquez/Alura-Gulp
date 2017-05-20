46bcc5a81bd7f2095c14de0de27ec6df4660c4e9
Bernardo Blasquez Taucei <Bernardo Blasquez Taucei>
2017-05-20 10:22:36 -0300

    configuração da task de minificação do js com o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,17 @@ var gulp = require('gulp');
 
 var imagemin = require('gulp-imagemin'),
     clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
     htmlReplace = require('gulp-html-replace'),
-    concat = require('gulp-concat');
+    uglify = require('gulp-uglify');
 
 
 gulp.task('default', ['copy'], function(){
-    gulp.start('minifica-img', 'concat-js', 'replace-html');
+    gulp.start('minifica-img', 'concat-uglify-js', 'replace-html');
     //todos executados de forma assíncrona
 })
 
-/* BUILD: minifica arquivos de imagem
+/* MINIFICA: minifica arquivos de imagem
 --------------------------------------------------------------------*/
     gulp.task('minifica-img', function(){
 
@@ -40,17 +41,21 @@ gulp.task('default', ['copy'], function(){
     });
 
 
-/* CONCAT: concatena arquivos: css e js
+/* CONCAT e UGLIFY: concatena e minifica arquivos css e js
 --------------------------------------------------------------------*/
-    gulp.task('concat-js', function() {
+    gulp.task('concat-uglify-js', function() {
 
         //abaixo é passado vários scripts em ordem, pois aqui a ordem é importante
 
         gulp.src(['dist/src/js/jquery.js','dist/src/js/home.js','dist/src/js/produtos.js'])
             .pipe(concat('allScripts.js'))
+            .pipe(uglify())
             .pipe(gulp.dest('dist/src/js'))
     });
 
+    // OBS:
+    // - A concatenação e a minificação ocorre toda em memória, diferente do que ocorre no GRUNT
+    // onde primeiro ele escreve os arquivos e depois efetua ouras tasks em cima do que foi escrito.
 
 /* HTML REPLACE: faz alterações na  página HTML - concatena chamadas
 --------------------------------------------------------------------*/
