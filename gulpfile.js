e91d838c73cf12521b284b3eb216741c774d6b48
Bernardo Blasquez Taucei <Bernardo Blasquez Taucei>
2017-05-20 11:48:33 -0300

    Configuração da task do usemin. Ela substirui  as tasks: concat, uglify, cssmin e htmlreplace

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,15 @@ var gulp = require('gulp');
 
 var imagemin = require('gulp-imagemin'),
     clean = require('gulp-clean'),
+    usemin = require('gulp-usemin'),
     concat = require('gulp-concat'),
     htmlReplace = require('gulp-html-replace'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    cssmin = require('gulp-cssmin');
 
 
 gulp.task('default', ['copy'], function(){
-    gulp.start('minifica-img', 'concat-uglify-js', 'replace-html');
+    gulp.start('minifica-img', 'usemin');
     //todos executados de forma assíncrona
 })
 
@@ -41,32 +43,21 @@ gulp.task('default', ['copy'], function(){
     });
 
 
-/* CONCAT e UGLIFY: concatena e minifica arquivos css e js
---------------------------------------------------------------------*/
-    gulp.task('concat-uglify-js', function() {
+/* USEMIN: simplifica a utilização dos modulos CONCAT, UGLIFY, CSSMIN e HTMLREPLACE
+------------------------------------------------------------------------------------*/
+// CONCAT e UGLIFY: concatena e minifica arquivos css e js
+// CSSMIN: minifica arquivos CSS
+// HTMLREPLACE: faz alterações na  página HTML concatenanso chamadas de script ou
+//              estilos em uma só.
 
-        //abaixo é passado vários scripts em ordem, pois aqui a ordem é importante
-
-        gulp.src(['dist/src/js/jquery.js','dist/src/js/home.js','dist/src/js/produtos.js'])
-            .pipe(concat('allScripts.js'))
-            .pipe(uglify())
-            .pipe(gulp.dest('dist/src/js'))
-    });
-
-    // OBS:
-    // - A concatenação e a minificação ocorre toda em memória, diferente do que ocorre no GRUNT
-    // onde primeiro ele escreve os arquivos e depois efetua ouras tasks em cima do que foi escrito.
-
-/* HTML REPLACE: faz alterações na  página HTML - concatena chamadas
---------------------------------------------------------------------*/
-    gulp.task('replace-html', function(){
-        gulp.src('dist/**/*.html')
-            .pipe(htmlReplace({
-                js:'js/allScripts.js'
+    gulp.task('usemin', function(){
+        gulp.src('dist/src/**/*.html')
+            .pipe(usemin({
+                'js':[uglify],
+                'css':[cssmin]
             }))
             .pipe(gulp.dest('dist'))
-    });
-
+    })
 
 // Formato da configuração de uma task
 gulp.task('nomeDaTarefa', function() {
